30e65c221b0b0ab27193767acc8e68d9ac7bd39b
SonoPG <pietro.gabriel.giambanco@gmail.com>
2026-04-12 10:45:38 +0200

    Make the game work locally

--- worker.js
+++ worker.js
@@ -15,14 +15,14 @@ self.addEventListener("fetch", (event) => {
   if (levelID < 0) {
     if (url.pathname.includes("1.txt")) {
       event.respondWith(
-        fetch(`/geometrydashdotcom/game/assets/levels/${levelID}.txt`)
+        fetch(`./assets/levels/${levelID}.txt`)
       );
       return;
     }
 
     if (url.pathname.includes("StereoMadness.mp3")) {
       event.respondWith(
-        fetch(`/geometrydashdotcom/game/assets/music/${levelID}.mp3`)
+        fetch(`./assets/music/${levelID}.mp3`)
       );
       return;
     }
